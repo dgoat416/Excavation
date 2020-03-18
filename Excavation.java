339ea2845fc2b435b3b2a85d11fa733f45913414
dgoat416 <55709445+dgoat416@users.noreply.github.com>
2020-03-18 01:55:20 -0700

    Update to Improve Performance (Speed)
    - readInputFile changed to read input file and simultaneously calculate the sums of all columns and rows & return the optimal windows between left and right and top and bottom
    - Everything changed to use arrays instead of arrayLists
    - findLargestWindowLR name changed to findOptimalWindow and changed to accept arrays instead of arrayLists
    - added logic to check the time of the running algorithm which doesn't seem to be accurate because somehow it was faster before using arrayLists, and all those loops I had
    
    NEXT PLAN OF ACTION IF PERFORMANCE IS NOT IMPROVED ENOUGH:
    - calculate sums for rows and columns after reading the file
    - check times of inputFile algorithms (using tokenizer or Scanner.nextInt)
    - check times of all loops etc. and try and optimize

--- Excavation.java
+++ Excavation.java
@@ -122,7 +122,7 @@ public class Excavation
 	 */
 	public static void readInputFile(ArrayList<ArrayList<Integer>> _inputArr)
 	{
-		File inFile = new File("input (2).txt");
+		File inFile = new File("input.txt");
 		int iRow = 0;
 		int endingIndex = 0;
 		String oneLine = "";
@@ -181,6 +181,101 @@ public class Excavation
 	}
 
 	/**
+	 * Read input file and populate the input array
+	 * @param _inputArr = 2d array to hold the contents of the input file
+	 * @return
+	 * 				= an array of point variables 
+	 * 					[0] = the optimal left-right window
+	 * 					[1] = the optimal top-bottom window
+	 */
+	public static Point[] readInputFile(int[][] _inputArr)
+	{
+		int[] colSums = null;
+		int[] rowSums = null;
+		Point[] optimalWindows = new Point[2];
+		int currInt = 0;
+		File inFile = new File("input.txt");
+		Scanner scan = null;
+
+		try
+		{
+			scan = new Scanner(inFile);
+			final int N = scan.nextInt();
+			
+			// declare arrays since we now know the size
+			_inputArr = new int[N][N];
+			colSums = new int[N];
+			rowSums = new int[N];
+			
+			// compute the sums of rows and cols as the input is read
+			for (int iRow = 0; iRow < N; iRow++)
+			{
+				for(int iCol = 0; iCol < N; iCol++)
+				{
+					// store current integer value in array
+					currInt = scan.nextInt();
+					_inputArr[iRow][iCol] = currInt;
+					
+					// compute sum for row 
+					rowSums[iRow] += currInt;
+					
+					// compute col sum
+					colSums[iCol] += currInt;
+				}					
+			}
+			
+			// get left-right optimal window
+			Point leftRightWindow = findOptimalWindow(rowSums);
+			Point topBottomWindow = new Point(0,0);
+			
+			// 2A. REDEFINE THE COL SUMS ARRAY BASED ON 
+			// THE OPTIMAL ROW WINDOW IF NECESSARY
+			
+			// this is how big to make the array of colSums after the 
+			// optimalWindow for left to right is found
+			int colSumsLength = (leftRightWindow.y - leftRightWindow.x) + 1;
+	
+			// no need for new array because it is the same array
+			if (colSumsLength == colSums.length)
+				topBottomWindow = findOptimalWindow(colSums);
+			
+			// create and populate new array
+			else
+			{
+				int[] newColSums = new int[colSumsLength];
+				int index = leftRightWindow.x;
+				
+				// populate new array
+				for (int iRow = 0; iRow < colSumsLength; iRow++)
+				{
+					newColSums[iRow] = colSums[index];
+					
+					//update
+					index++;
+				}
+				
+				// get top-bottom optimal window
+				topBottomWindow = findOptimalWindow(newColSums);
+			}
+			
+			// store the optimalWindow Rectangle coordinates
+			optimalWindows[0] = leftRightWindow;
+			optimalWindows[1] = topBottomWindow;
+		}
+		catch(FileNotFoundException FNF)
+		{
+			FNF.printStackTrace();
+			System.out.println("File is not found.");
+		}
+		finally
+		{
+			scan.close();
+		}
+		
+		return optimalWindows;
+	}
+	
+	/**
 	 * Write output file 
 	 * @param _leftRightWindow = optimal left and right window
 	 * @param _topBottomWindow = optimal top and bottom window
@@ -193,14 +288,14 @@ public class Excavation
 		try 
 		{
 			pWriter = new PrintWriter(outFile);
-			
+
 			Point temp = new Point(1,1);
 			_leftRightWindow.x = _leftRightWindow.x + temp.x;
 			_leftRightWindow.y = _leftRightWindow.y + temp.y;
 			_topBottomWindow.x = _topBottomWindow.x + temp.x;
 			_topBottomWindow.y = _topBottomWindow.y + temp.y;
-			
-			
+
+
 			pWriter.write(_leftRightWindow.x + " " + _topBottomWindow.x + "\n");
 			pWriter.write(_leftRightWindow.y + " " + _topBottomWindow.y + "\n");
 		}
@@ -212,7 +307,7 @@ public class Excavation
 
 		pWriter.close();
 	}
-	
+
 	/**
 	 * Finds the largest Sum in an ArrayList
 	 * @param oneRow = one Row of a multidimensional arrayList
@@ -281,6 +376,52 @@ public class Excavation
 		return new Point(globalTempLeft, globalTempRight);
 	}
 
+	/**
+	 * Find the window that holds the largestSum between left and right
+	 * @param oneRow = the row with the largest size
+	 * @return
+	 * 				=  point that holds the size of the window that maximizes sum between
+	 *	 				left side of window(first coordinate) of oneRow
+	 *	 				right side of window(second coordinate) of oneRow
+	 */
+	public static Point findOptimalWindow(int[] oneRow)
+	{		
+		int localMax = 0;
+		int globalMax = Integer.MIN_VALUE;
+		int localTempLeft = 0;
+		int localTempRight = 0;
+		int globalTempLeft = 0;
+		int globalTempRight = 0;
+
+		for(int i = 0; i < oneRow.length; i++)
+		{
+			// find local max by comparing the current row 
+			// and local max + current row and determining the max 
+			localMax = Integer.max(oneRow[i], localMax + oneRow[i]);
+
+			// only change tempLeft if starting the new max from oneRow[i] & not at the end
+			if (oneRow[i] == localMax && i != oneRow.length - 1) 
+			{
+				localTempLeft = i;
+				localTempRight = i;
+			}
+
+			// change tempRight every iteration to update the new window
+			else 
+				localTempRight = i;
+
+			if (localMax > globalMax)
+			{
+				globalMax = localMax;
+				globalTempLeft = localTempLeft;
+				globalTempRight = localTempRight;
+			}
+		}
+
+		return new Point(globalTempLeft, globalTempRight);
+	}
+
+	
 	/**
 	 * Find the window that holds the largestSum between top and bottom
 	 * @param _maxes = the maxes of each line at the width specified by leftRight
@@ -414,7 +555,7 @@ public class Excavation
 		return rotatedMatrix;
 	}
 
-	
+
 	/**
 	 * Transforms the parameter matrix to its transpose
 	 * @param _inputArr = the original array from input file
@@ -426,17 +567,17 @@ public class Excavation
 		ArrayList<ArrayList<Integer>> rotatedArr = new ArrayList<ArrayList<Integer>>();
 		//int temp = 0;
 
-//		// works for square matrix & uses same arrayList (no extra space)
-//		for (int i = 0; i < _inputArr.size(); i++)
-//		{
-//			for (int j = i + 1; j < _inputArr.get(i).size(); j++)
-//			{
-//				temp = _inputArr.get(i).get(j);
-//				_inputArr.get(i).set(j, _inputArr.get(j).get(i));
-//				_inputArr.get(j).set(i, temp);
-//			}
-//		}
-		
+		//		// works for square matrix & uses same arrayList (no extra space)
+		//		for (int i = 0; i < _inputArr.size(); i++)
+		//		{
+		//			for (int j = i + 1; j < _inputArr.get(i).size(); j++)
+		//			{
+		//				temp = _inputArr.get(i).get(j);
+		//				_inputArr.get(i).set(j, _inputArr.get(j).get(i));
+		//				_inputArr.get(j).set(i, temp);
+		//			}
+		//		}
+
 		// works for any square or rectangular matrix
 		for (int ogRow = 0; ogRow < _inputArr.size(); ogRow++)
 		{
@@ -445,13 +586,13 @@ public class Excavation
 				// instantiate a new row for as many columns as the original matrix
 				if (ogRow == 0)
 					rotatedArr.add(new ArrayList<Integer>());
-				
+
 				// for each ogCol (or row of rotatedArr) add the 
 				// corresponding value of the transpose
 				rotatedArr.get(ogCol).add(ogRow, _inputArr.get(ogRow).get(ogCol));
 			}
 		}
-		
+
 		return rotatedArr;
 	}
 
@@ -620,109 +761,121 @@ public class Excavation
 		return _pObjArr.get(most.x).aPoint.iPoint;
 	}
 
-	
+
 	public static void main(String[] args)
 	{
-		// array to hold the contents of the input file
-		ArrayList<ArrayList<Integer>> inputArr = new ArrayList<ArrayList<Integer>>();
+//		final long startTime = System.currentTimeMillis();
+//				// array to hold the contents of the input file
+//				ArrayList<ArrayList<Integer>> inputArr = new ArrayList<ArrayList<Integer>>();
+//		
+//				
+//				// holds the points
+//				Point leftRightWindow = new Point(0,0);
+//				Point topBottomWindow = new Point(0,0);
+//		
+//				// populate inputArr
+//				readInputFile(inputArr);
+//				
+//				// grab n and remove it
+//				int n = inputArr.get(0).get(0);
+//				inputArr.remove(0);
+//		
+//				// STEP 1
+//				// figure out how to find the biggest sum within oneLine (left and right) 
+//				// for each row of the inputArr
+//			
+//				int oneRowSum = 0;
+//				ArrayList<Integer> sums = new ArrayList<Integer>();
+//				
+//				for (int i = 0; i < n; i++)
+//				{
+//					for (int j = 0; j < n; j++)
+//						oneRowSum += inputArr.get(i).get(j);
+//					
+//					sums.add(oneRowSum);
+//					oneRowSum = 0;
+//				}
+//				
+//				leftRightWindow = findLargestWindowLR(sums);
+//		
+//				// STEP 2
+//				// CREATE A SUBARRAY OR SUB ARRAYLIST with rows size temp (x to y width)
+//				// & find the optimal window (maximum sum) of the columns of this subArray/List
+//				// which is essentially the top and bottom portion of the optimal rectangle 
+//				// This can be found by finding the optimal window of the transpose of 
+//				// the sub array/list  and report the top and bottom
+//				// window that has the largest sum
+//		
+//				
+//				// shave off the part that is already not optimal (left to right)
+//				
+//				// 2A. CUT OFF THE BEGINNING OF ROWS 
+//				// (MEANING COLS AT THE BEGINNING OF THE ROWS) IF NECESSARY 
+//				
+//				// TESTING PURPOSES
+//				//leftRightWindow.
+//				// END TESTING
+//				
+//				// delete temp.x - 0 rows for first loop
+//				// delete rowsize - tempWindow.y rows for second loop
+//				Point tempWindow = leftRightWindow;
+//				tempWindow = new Point(leftRightWindow.x, (inputArr.get(0).size() - 1 ) - leftRightWindow.y);
+//				
+//				for (int iCol = 0; iCol < tempWindow.x; iCol++)
+//				{
+//					// cut off the beginning columns of jRow if necessary
+//					for (int jRow = 0; jRow < inputArr.size(); jRow++)
+//						inputArr.get(jRow).remove(0);
+//				}
+//		
+//				// 2B. CUT OFF THE END OF THE ROWS 
+//				// MEANING CUT OFF THE COLS AT THE END OF THE ROWS
+//				// IF NECESSARY
+//				for (int iCol = 0; iCol < tempWindow.y; iCol++)
+//				{
+//					int iColMax = inputArr.get(0).size() - 1;
+//					// for every row jRow eliminate column iCol
+//					for (int jRow = 0; jRow < inputArr.size(); jRow++)
+//						inputArr.get(jRow).remove(iColMax);
+//				}
+//		
+//				// 2C. FIND THE TRANSPOSE AND FIND THE OPTIMAL WINDOW
+//				// FOR THE TRANSPOSE (WHICH WILL BE TOP AND BOTTOM WINDOW)
+//				ArrayList<ArrayList<Integer>> transpose = new ArrayList<ArrayList<Integer>>();
+//				transpose = toTranspose(inputArr);
+//				
+//				// reset all necessary containers & variables
+//				oneRowSum = 0;
+//				sums.clear();
+//		
+//				// get the sum for every row of the transpose (essentially top and bottom)
+//				for (int i = 0; i < transpose.size(); i++)
+//				{
+//					for (int j = 0; j < transpose.get(i).size(); j++)
+//						oneRowSum += transpose.get(i).get(j);
+//					
+//					// add to the list of sums of each row and reset oneRowSum 
+//					sums.add(oneRowSum);
+//					oneRowSum = 0;			
+//				}
+//				
+//				// find the optimal window for top and bottom
+//				topBottomWindow = findLargestWindowLR(sums);
+//				
+//				// write the optimal window to output file
+//				writeToOutputFile(leftRightWindow, topBottomWindow);
+				
 
 		
-		// holds the points
-		Point leftRightWindow = new Point(0,0);
-		Point topBottomWindow = new Point(0,0);
+		final long startTime = System.currentTimeMillis();
+		int [][] inputArr = null;
+		Point[] optimalWindow = readInputFile(inputArr);
+		writeToOutputFile(optimalWindow[0], optimalWindow[1]);
+		
+		final long endTime = System.currentTimeMillis();
 
-		// populate inputArr
-		readInputFile(inputArr);
-		
-		// grab n and remove it
-		int n = inputArr.get(0).get(0);
-		inputArr.remove(0);
+		System.out.println("Total execution time: " + (endTime - startTime));
 
-		// STEP 1
-		// figure out how to find the biggest sum within oneLine (left and right) 
-		// for each row of the inputArr
-	
-		int oneRowSum = 0;
-		ArrayList<Integer> sums = new ArrayList<Integer>();
-		
-		for (int i = 0; i < n; i++)
-		{
-			for (int j = 0; j < n; j++)
-				oneRowSum += inputArr.get(i).get(j);
-			
-			sums.add(oneRowSum);
-			oneRowSum = 0;
-		}
-		
-		leftRightWindow = findLargestWindowLR(sums);
-
-		// STEP 2
-		// CREATE A SUBARRAY OR SUB ARRAYLIST with rows size temp (x to y width)
-		// & find the optimal window (maximum sum) of the columns of this subArray/List
-		// which is essentially the top and bottom portion of the optimal rectangle 
-		// This can be found by finding the optimal window of the transpose of 
-		// the sub array/list  and report the top and bottom
-		// window that has the largest sum
-
-		
-		// shave off the part that is already not optimal (left to right)
-		
-		// 2A. CUT OFF THE BEGINNING OF ROWS 
-		// (MEANING COLS AT THE BEGINNING OF THE ROWS) IF NECESSARY 
-		
-		// TESTING PURPOSES
-		//leftRightWindow.
-		// END TESTING
-		
-		// delete temp.x - 0 rows for first loop
-		// delete rowsize - tempWindow.y rows for second loop
-		Point tempWindow = leftRightWindow;
-		tempWindow = new Point(leftRightWindow.x, (inputArr.get(0).size() - 1 ) - leftRightWindow.y);
-		
-		for (int iCol = 0; iCol < tempWindow.x; iCol++)
-		{
-			// cut off the beginning columns of jRow if necessary
-			for (int jRow = 0; jRow < inputArr.size(); jRow++)
-				inputArr.get(jRow).remove(0);
-		}
-
-		// 2B. CUT OFF THE END OF THE ROWS 
-		// MEANING CUT OFF THE COLS AT THE END OF THE ROWS
-		// IF NECESSARY
-		for (int iCol = 0; iCol < tempWindow.y; iCol++)
-		{
-			int iColMax = inputArr.get(0).size() - 1;
-			// for every row jRow eliminate column iCol
-			for (int jRow = 0; jRow < inputArr.size(); jRow++)
-				inputArr.get(jRow).remove(iColMax);
-		}
-
-		// 2C. FIND THE TRANSPOSE AND FIND THE OPTIMAL WINDOW
-		// FOR THE TRANSPOSE (WHICH WILL BE TOP AND BOTTOM WINDOW)
-		ArrayList<ArrayList<Integer>> transpose = new ArrayList<ArrayList<Integer>>();
-		transpose = toTranspose(inputArr);
-		
-		// reset all necessary containers & variables
-		oneRowSum = 0;
-		sums.clear();
-
-		// get the sum for every row of the transpose (essentially top and bottom)
-		for (int i = 0; i < transpose.size(); i++)
-		{
-			for (int j = 0; j < transpose.get(i).size(); j++)
-				oneRowSum += transpose.get(i).get(j);
-			
-			// add to the list of sums of each row and reset oneRowSum 
-			sums.add(oneRowSum);
-			oneRowSum = 0;			
-		}
-		
-		// find the optimal window for top and bottom
-		topBottomWindow = findLargestWindowLR(sums);
-		
-		// write the optimal window to output file
-		writeToOutputFile(leftRightWindow, topBottomWindow);
-		
 	}
 
 }
